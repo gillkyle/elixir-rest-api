37f79d929fe09afc09e4071c8bd53b870626f62e
gillkyle <kylerobertgill@gmail.com>
2018-10-16 19:58:40 -0600

    init

---
+++ web/models/user.ex
@@ -0,0 +1,4 @@
+# web/models/user.ex
+defmodule ApiExample.User do
+  defstruct [:id, :name, :email, :password, :power]
+end
---
+++ web/router.ex
@@ -0,0 +1,33 @@
+defmodule ApiExample.Router do
+  use ApiExample.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", ApiExample do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  scope "/api/v1", ApiExample do
+    pipe_through :api
+
+    get "/users", UserController, :index
+    get "/users/:id", UserController, :show
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", ApiExample do
+  #   pipe_through :api
+  # end
+end
